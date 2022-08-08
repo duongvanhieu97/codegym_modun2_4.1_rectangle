ecf50b8df3ea54c7103121ea49110529c6734f10
Duonghieu97st <Duonghieu97st@gmail.com>
2022-08-08 13:52:39 +0700

    update main

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập giá trị width");
+        double width = scanner.nextDouble();
+        System.out.println("Nhập giá trị height");
+        double height = scanner.nextDouble();
+        Rectangle rectangle = new Rectangle(width,height);
+        System.out.println("Độ dài, chiều rộng của hình chữ nhật \n" + rectangle.display());
+        System.out.println("Chu vi hình chữ nhật: " + rectangle.getPerimeter());
+        System.out.println("Diện tích hình chữ nhật: " + rectangle.getArea());
+    }
 }
